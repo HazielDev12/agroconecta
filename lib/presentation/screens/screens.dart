df76f6344c8c1c180784c147fdfc571454b5438f
Abraham Gutiérrez <habrahamgutierrez289@gmail.com>
2025-10-23 15:42:33 -0500

    se cambio el nombre a ingles

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,2 +1,2 @@
 export 'package:agroconecta/presentation/screens/login_screen.dart';
-export 'package:agroconecta/presentation/screens/menu_principal.dart';
+export 'package:agroconecta/presentation/screens/home_page.dart';
