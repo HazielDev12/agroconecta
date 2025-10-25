4b26c8649b64e9790b0559b5feb5d7eb84a76345
Haziel Sanchez <hazieldev12@gmail.com>
2025-10-25 14:08:39 -0500

    Se agregó SingleChildScrollView para evitar errores del scroll

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -54,92 +54,94 @@ class _RegisterView extends StatelessWidget {
               ],
             ),
             height: 650, //altura de la tarjeta
-            child: Column(
-              children: [
-                SvgPicture.asset(
-                  'assets/images/logo_con_texto.svg',
-                  height: 200, // ajusta según tu diseño
-                ),
-                const SizedBox(height: 16),
-                //Texto principal
-                const Text(
-                  'Tecnología que siembra futuro.',
-                  style: TextStyle(fontSize: 14, color: Colors.black54),
-                ),
-
-                const SizedBox(height: 30),
-
-                //Campo de usuario/correo
-                const CustomTextFormField(
-                  label: 'Correo',
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                const SizedBox(height: 16),
-
-                //Campo contraseña
-                const CustomTextFormField(
-                  label: 'Contraseña',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 10),
-                //Links inferiores
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: GestureDetector(
-                    onTap: () => context.push('/forgot-password'),
-                    child: const Text(
-                      '¿Olvidaste tu contraseña?',
-                      style: TextStyle(
-                        color: Color(0xFF22A788),
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SvgPicture.asset(
+                    'assets/images/logo_con_texto.svg',
+                    height: 200, // ajusta según tu diseño
                   ),
-                ),
-                const SizedBox(height: 2),
-                //Boton principal
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      context.push('/home');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: colorList[0],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadiusGeometry.circular(10),
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 14),
-                    ),
-                    child: const Text(
-                      'Iniciar Sesión',
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
+                  const SizedBox(height: 16),
+                  //Texto principal
+                  const Text(
+                    'Tecnología que siembra futuro.',
+                    style: TextStyle(fontSize: 14, color: Colors.black54),
                   ),
-                ),
-
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      '¿No tienes cuenta? ',
-                      style: TextStyle(color: Colors.black54),
-                    ),
-                    GestureDetector(
-                      onTap: () => context.push('/sign-up'),
+              
+                  const SizedBox(height: 30),
+              
+                  //Campo de usuario/correo
+                  const CustomTextFormField(
+                    label: 'Correo',
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(height: 16),
+              
+                  //Campo contraseña
+                  const CustomTextFormField(
+                    label: 'Contraseña',
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 10),
+                  //Links inferiores
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: GestureDetector(
+                      onTap: () => context.push('/forgot-password'),
                       child: const Text(
-                        'Regístrate aquí',
+                        '¿Olvidaste tu contraseña?',
                         style: TextStyle(
                           color: Color(0xFF22A788),
                           fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline,
                         ),
                       ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const SizedBox(height: 2),
+                  //Boton principal
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        context.push('/home');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: colorList[0],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(10),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 14),
+                      ),
+                      child: const Text(
+                        'Iniciar Sesión',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  ),
+              
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        '¿No tienes cuenta? ',
+                        style: TextStyle(color: Colors.black54),
+                      ),
+                      GestureDetector(
+                        onTap: () => context.push('/sign-up'),
+                        child: const Text(
+                          'Regístrate aquí',
+                          style: TextStyle(
+                            color: Color(0xFF22A788),
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
