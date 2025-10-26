a064713c62e07c36981b4e8ee8344f5a4e2fe6b7
Haziel Sanchez <hazieldev12@gmail.com>
2025-10-25 22:25:06 -0500

    Separación de clases

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -11,7 +11,50 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      child: Scaffold(body: const _LoginForm()),
+      child: Scaffold(
+        body: DecoratedBox(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/cultivo.webp'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: SafeArea(child: Center(child: const _LoginCard())),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginCard extends StatelessWidget {
+  const _LoginCard();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(
+        left: 24,
+        right: 24,
+        top: 24,
+        bottom:
+            MediaQuery.of(context).viewInsets.bottom +
+            24, //Evita recortes con teclado
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFFF6F8E8).withValues(alpha: 0.90),
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.2),
+              blurRadius: 6,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        padding: const EdgeInsets.all(24),
+        child: const _LoginForm(), // contenido del form
+      ),
     );
   }
 }
@@ -23,129 +66,91 @@ class _LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     final textStyles = Theme.of(context).textTheme;
 
-    return Stack(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/cultivo.webp'),
-              fit: BoxFit.cover,
-            ),
+    return Form(
+      //Contenido Centrado
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SvgPicture.asset(
+            'assets/images/logo_con_texto.svg',
+            height: 200, // ajusta según tu diseño
           ),
-        ),
+          const SizedBox(height: 16),
+          //Texto principal
+          const Text(
+            'Tecnología que siembra futuro.',
+            style: TextStyle(fontSize: 14, color: Colors.black54),
+          ),
 
-        //Contenido Centrado
-        Center(
-          child: Container(
-            padding: const EdgeInsets.all(25),
-            margin: const EdgeInsets.symmetric(horizontal: 25),
-            decoration: BoxDecoration(
-              color: colorList[2].withValues(alpha: 0.7),
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withValues(
-                    alpha: 0.2,
-                  ), //Color de la sombra
-                  blurRadius: 2, //Difuminación de la sombra
-                  offset: const Offset(
-                    0,
-                    4,
-                  ), //Indica que la sombra se desplaza hacia abajo
+          const SizedBox(height: 30),
+
+          //Campo de usuario/correo
+          const CustomTextFormField(
+            label: 'Correo',
+            keyboardType: TextInputType.emailAddress,
+          ),
+          const SizedBox(height: 16),
+
+          //Campo contraseña
+          const CustomTextFormField(label: 'Contraseña', obscureText: true),
+          const SizedBox(height: 10),
+          //Links inferiores
+          Align(
+            alignment: Alignment.centerRight,
+            child: GestureDetector(
+              onTap: () => context.push('/forgot-password'),
+              child: const Text(
+                '¿Olvidaste tu contraseña?',
+                style: TextStyle(
+                  color: Color(0xFF22A788),
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
-            height: 650, //altura de la tarjeta
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  SvgPicture.asset(
-                    'assets/images/logo_con_texto.svg',
-                    height: 200, // ajusta según tu diseño
-                  ),
-                  const SizedBox(height: 16),
-                  //Texto principal
-                  const Text(
-                    'Tecnología que siembra futuro.',
-                    style: TextStyle(fontSize: 14, color: Colors.black54),
-                  ),
-
-                  const SizedBox(height: 30),
-
-                  //Campo de usuario/correo
-                  const CustomTextFormField(
-                    label: 'Correo',
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(height: 16),
-
-                  //Campo contraseña
-                  const CustomTextFormField(
-                    label: 'Contraseña',
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 10),
-                  //Links inferiores
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: GestureDetector(
-                      onTap: () => context.push('/forgot-password'),
-                      child: const Text(
-                        '¿Olvidaste tu contraseña?',
-                        style: TextStyle(
-                          color: Color(0xFF22A788),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 2),
-                  //Boton principal
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: colorList[0],
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadiusGeometry.circular(10),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 14),
-                      ),
-                      child: const Text(
-                        'Iniciar Sesión',
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                    ),
-                  ),
-
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        '¿No tienes cuenta? ',
-                        style: TextStyle(color: Colors.black54),
-                      ),
-                      GestureDetector(
-                        onTap: () => context.push('/sign-up'),
-                        child: const Text(
-                          'Regístrate aquí',
-                          style: TextStyle(
-                            color: Color(0xFF22A788),
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
               ),
             ),
           ),
-        ),
-      ],
+          const SizedBox(height: 2),
+          //Boton principal
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: colorList[0],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(10),
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 14),
+              ),
+              child: const Text(
+                'Iniciar Sesión',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                '¿No tienes cuenta? ',
+                style: TextStyle(color: Colors.black54),
+              ),
+              GestureDetector(
+                onTap: () => context.push('/sign-up'),
+                child: const Text(
+                  'Regístrate aquí',
+                  style: TextStyle(
+                    color: Color(0xFF22A788),
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
