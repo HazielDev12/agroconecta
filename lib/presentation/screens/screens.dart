46c14df70ee9ef0c6662b8461d9d9b0977d865b2
Haziel <155182311+HazielDev12@users.noreply.github.com>
2025-10-26 01:40:13 -0500

    Remove unused import for convocatoria screen

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,1 @@
-export 'package:agroconecta/presentation/screens/login_screen.dart';
 export 'package:agroconecta/presentation/screens/home_page.dart';
-export 'package:agroconecta/presentation/screens/convocatoria.dart';
