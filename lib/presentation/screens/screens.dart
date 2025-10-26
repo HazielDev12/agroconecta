7c64cd391b424a629d885b548d40b41271ec3ff3
Abraham Gutiérrez <habrahamgutierrez289@gmail.com>
2025-10-25 20:00:16 -0500

    se separo la pagina de error a una independiente

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -2,3 +2,4 @@ export 'package:agroconecta/presentation/screens/login_screen.dart';
 export 'package:agroconecta/presentation/screens/home_page.dart';
 export 'package:agroconecta/presentation/screens/convocatoria.dart';
 export 'package:agroconecta/presentation/screens/calendar_page.dart';
+export 'package:agroconecta/presentation/screens/not_found.dart';
