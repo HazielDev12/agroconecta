a4870104fb25bafb1ce1e92c37a7cc528459dd32
Haziel Sanchez <hazieldev12@gmail.com>
2025-10-28 13:23:53 -0500

    Implementación del AuthDataSource-Login

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -1,10 +1,12 @@
-
-
-
+import 'package:agroconecta/config/config.dart';
 import 'package:agroconecta/features/auth/domain/datasources/auth_datasource.dart';
 import 'package:agroconecta/features/auth/domain/entities/user.dart';
+import 'package:agroconecta/features/auth/infrastructure/infrastructure.dart';
+import 'package:dio/dio.dart';
 
 class AuthDataSourceImpl extends AuthDataSource {
+  final dio = Dio(BaseOptions(baseUrl: Enviroment.apiUrl));
+
   @override
   Future<User> checkAuthStatus(String token) {
     // TODO: implement checkAuthStatus
@@ -12,9 +14,17 @@ class AuthDataSourceImpl extends AuthDataSource {
   }
 
   @override
-  Future<User> login(String username, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<User> login(String curp, String password) async {
+    try {
+      final response = await dio.post(
+        '/login',
+        data: {'curp': curp, 'password': password},
+      );
+
+      final user = UserMapper.userJsonToEntity(response.data);
+      return user;
+    } catch (e) {
+      throw WrongCredentials();
+    }
   }
-  
 }
--- lib/features/auth/infrastructure/repositories/auth_repository_impl.dart
+++ lib/features/auth/infrastructure/repositories/auth_repository_impl.dart
@@ -1,14 +1,11 @@
-
 import 'package:agroconecta/features/auth/domain/domain.dart';
 import 'package:agroconecta/features/auth/infrastructure/datasources/auth_datasource_impl.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
-
   final AuthDataSource dataSource;
 
-  AuthRepositoryImpl(
-    AuthDataSource? dataSource
-  ) : dataSource = dataSource ?? AuthDataSourceImpl();
+  AuthRepositoryImpl(AuthDataSource? dataSource)
+    : dataSource = dataSource ?? AuthDataSourceImpl();
 
   @override
   Future<User> checkAuthStatus(String token) {
@@ -16,8 +13,7 @@ class AuthRepositoryImpl extends AuthRepository {
   }
 
   @override
-  Future<User> login(String username, String password) {
-    return dataSource.login(username, password);
+  Future<User> login(String curp, String password) {
+    return dataSource.login(curp, password);
   }
-  
 }
