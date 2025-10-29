7388c4778bee699d6f2c89f123ee25e71d226e32
Haziel Sanchez <hazieldev12@gmail.com>
2025-10-29 09:12:38 -0500

    Se modificó el RegExp

--- lib/features/shared/infrastructure/inputs/curp.dart
+++ lib/features/shared/infrastructure/inputs/curp.dart
@@ -3,19 +3,20 @@ import 'package:formz/formz.dart';
 enum CurpError { empty, length, format }
 
 class Curp extends FormzInput<String, CurpError> {
-  const Curp.pure() : super.pure('');
-  const Curp.dirty(String value) : super.dirty(value);
-
   // CURP oficial (mayúsculas), incluye entidades federativas y estructura.
-  static final _curpReg = RegExp(
+  static final _curpReg = RegExp(r'^[A-Z]{4}\d{6}[HM]{1}[A-Z]{5}[A-Z0-9]{2}$');
+  /*   static final _curpReg = RegExp(
     r'^[A-Z][AEIOUX][A-Z]{2}\d{2}'
     r'(0[1-9]|1[0-2])' // mes
     r'(0[1-9]|[12]\d|3[01])' // día
     r'[HM]'
     r'(AS|BC|BS|CC|CL|CM|CS|CH|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS)'
     r'[B-DF-HJ-NP-TV-Z]{3}'
-    r'[A-Z0-9]\d$',
-  );
+    r'[A-Z0-9]\d$'
+    );
+ */
+  const Curp.pure() : super.pure('');
+  const Curp.dirty(String value) : super.dirty(value);
 
   String? get errorMessage {
     if (isValid || isPure) return null;
@@ -26,7 +27,7 @@ class Curp extends FormzInput<String, CurpError> {
       return 'La CURP debe tener 18 caracteres';
     }
     if (displayError == CurpError.format) {
-      return 'CURP no válida (usa mayúsculas)';
+      return 'CURP no válida. Verifica tus datos.';
     }
     return null;
   }
